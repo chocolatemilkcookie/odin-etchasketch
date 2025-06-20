7a912196dd2d5df0228ba68179d697a44f301193
chocolatemilkcookie <187956214+chocolatemilkcookie@users.noreply.github.com>
2025-06-20 16:58:09 +0300

    Add a button that allows users to choose their desired grid size

--- etch.js
+++ etch.js
@@ -1,47 +1,66 @@
 let container = document.querySelector(".container");
+let gridBtn = document.querySelector("#gridbtn");
 
 let gridDimensions = 360;
-let gridSize = 50;
-let borderSize = (1 * gridSize) ;
-let squareSize = (gridDimensions- borderSize) /gridSize;
-rowSize = gridDimensions/gridSize
-console.log(squareSize);
+let gridSize;
+let borderSize;
+let squareSize;
+let rowSize;
+
+gridBtn.addEventListener("click" , () => {
+    let desiredSize = prompt("Enter desired grid squares per side");
+    gridSize = +desiredSize;
+    calculateGridDimensions();
+    createGrid();
+    console.log(gridSize);
+});
 
 
-// Creating a row
+function calculateGridDimensions(){
+
+    borderSize = (1 * gridSize) ;
+    squareSize = (gridDimensions- borderSize) /gridSize;
+    rowSize = gridDimensions/gridSize
+    console.log(squareSize);
+
+}
 
 
-for(let j = 0; j<gridSize; j++){
+function createGrid(){
 
-    let row = document.createElement("div");
+    // Creating a row
+
+    for(let j = 0; j<gridSize; j++){
+
+        let row = document.createElement("div");
+        
+        row.style.height = `${rowSize}px`//Will this be height or width depending on flex direction
+        row.style.width = `${gridDimensions}px`;
+        row.style.margin = "0px";
+        row.style.padding = "0px";
+        row.style.display = "flex";
     
-    row.style.height = `${rowSize}px`//Will this be height or width depending on flex direction
-    row.style.width = `${gridDimensions}px`;
-    row.style.margin = "0px";
-    row.style.padding = "0px";
-    row.style.display = "flex";
-
-    container.appendChild(row);
-
-    //  Creating squares
-
-    for(let i = 0; i< gridSize; i++){
-
-        let square = document.createElement("div");
+        container.appendChild(row);
     
-        square.style.width = `${squareSize}px`;
-        square.style.height = `${squareSize}px`;
-        square.style.border = "1px solid lightgray";
-        square.style.margin = "0px";
+        //  Creating squares
     
-        row.appendChild(square);
-
-        square.addEventListener("mouseenter", hover);
+        for(let i = 0; i< gridSize; i++){
     
+            let square = document.createElement("div");
+        
+            square.style.width = `${squareSize}px`;
+            square.style.height = `${squareSize}px`;
+            square.style.border = "1px solid lightgray";
+            square.style.margin = "0px";
+        
+            row.appendChild(square);
+    
+            square.addEventListener("mouseenter", hover); 
+        }  
     }
-
+    
+    function hover(e){
+        e.target.style.backgroundColor = "pink";
+    }
 }
 
-function hover(e){
-    e.target.style.backgroundColor = "pink";
-}
