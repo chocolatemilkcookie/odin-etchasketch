fcd627f14cd3f93d7755380b52bbacb3d8d1e913
chocolatemilkcookie <187956214+chocolatemilkcookie@users.noreply.github.com>
2025-06-20 13:50:22 +0300

    Make complete grid of size of choice

--- etch.js
+++ etch.js
@@ -1,18 +1,41 @@
 let container = document.querySelector(".container");
 
-let gridSize = 16;
-let squareSize = 360/gridSize;
+let gridDimensions = 360;
+let gridSize = 7;
+let borderSize = (1.5 * gridSize) ;
+let squareSize = (gridDimensions- borderSize) /gridSize;
 console.log(squareSize);
 
-for(let i = 0; i<= gridSize; i++){
 
-    let square = document.createElement("div");
+// Creating a row
 
-    square.style.width = `${squareSize}px`;
-    square.style.height = `${squareSize}px`;
-    square.style.border = "1px solid black";
-    square.style.margin = "0px";
 
-    container.appendChild(square);
+for(let j = 0; j<gridSize; j++){
+
+    let row = document.createElement("div");
+    
+    row.style.height = `${squareSize}px`//Will this be height or width depending on flex direction
+    row.style.width = `${gridDimensions}px`;
+    row.style.border = "1px solid black"
+    row.style.margin = "0px";
+    row.style.padding = "0px";
+    row.style.display = "flex";
+
+    container.appendChild(row);
+
+    //  Creating squares
+    
+    for(let i = 0; i< gridSize; i++){
+
+        let square = document.createElement("div");
+    
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+        square.style.border = "1px solid black";
+        square.style.margin = "0px";
+    
+        row.appendChild(square);
+    
+    }
 
 }
