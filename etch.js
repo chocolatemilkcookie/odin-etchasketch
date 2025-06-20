a651045c0f35852a3f5be78d8e1eff02ae8bba32
chocolatemilkcookie <187956214+chocolatemilkcookie@users.noreply.github.com>
2025-06-20 13:03:33 +0300

    Create a container div and add one square grid

---
+++ etch.js
@@ -0,0 +1,10 @@
+let container = document.querySelector(".container");
+
+let square = document.createElement("div");
+
+square.style.width = "36px";
+square.style.height = "36px";
+square.style.border = "1px solid black";
+
+
+container.appendChild(square);
