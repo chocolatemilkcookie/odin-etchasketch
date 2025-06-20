f755b25198de3e93995d164598b5af4289482359
chocolatemilkcookie <187956214+chocolatemilkcookie@users.noreply.github.com>
2025-06-20 16:33:03 +0300

    Allow user to draw on grid

--- etch.js
+++ etch.js
@@ -1,9 +1,10 @@
 let container = document.querySelector(".container");
 
 let gridDimensions = 360;
-let gridSize = 7;
-let borderSize = (1.5 * gridSize) ;
+let gridSize = 50;
+let borderSize = (1 * gridSize) ;
 let squareSize = (gridDimensions- borderSize) /gridSize;
+rowSize = gridDimensions/gridSize
 console.log(squareSize);
 
 
@@ -14,9 +15,8 @@ for(let j = 0; j<gridSize; j++){
 
     let row = document.createElement("div");
     
-    row.style.height = `${squareSize}px`//Will this be height or width depending on flex direction
+    row.style.height = `${rowSize}px`//Will this be height or width depending on flex direction
     row.style.width = `${gridDimensions}px`;
-    row.style.border = "1px solid black"
     row.style.margin = "0px";
     row.style.padding = "0px";
     row.style.display = "flex";
@@ -24,18 +24,24 @@ for(let j = 0; j<gridSize; j++){
     container.appendChild(row);
 
     //  Creating squares
-    
+
     for(let i = 0; i< gridSize; i++){
 
         let square = document.createElement("div");
     
         square.style.width = `${squareSize}px`;
         square.style.height = `${squareSize}px`;
-        square.style.border = "1px solid black";
+        square.style.border = "1px solid lightgray";
         square.style.margin = "0px";
     
         row.appendChild(square);
+
+        square.addEventListener("mouseenter", hover);
     
     }
 
+}
+
+function hover(e){
+    e.target.style.backgroundColor = "pink";
 }
